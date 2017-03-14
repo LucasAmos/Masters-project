3f77e1c38aabb9f318b40288a11161ca0fd6b854
lucas amos <xkv1922c@gmail.com>
2017-03-14 22:57:10 +0000

    db models

--- sensors/views.py
+++ sensors/views.py
@@ -1,6 +1,13 @@
 from flask import render_template
 
-from sensors import app
+from sensors import app, db
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 @app.route("/")
 def hello():
