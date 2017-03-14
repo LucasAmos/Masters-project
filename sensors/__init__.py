c3d051d3a051c1e160af55e38f126257e56455ac
lucas amos <xkv1922c@gmail.com>
2017-03-14 22:54:49 +0000

    database

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 
 if __name__ == "__main__":
@@ -8,3 +10,5 @@ if __name__ == "__main__":
 
 import sensors.views
 import sensors.database
+
+db = SQLAlchemy(app)
--- sensors/database.py
+++ sensors/database.py
@@ -1,6 +1,5 @@
 from sensors import app
 
-from flask.ext.sqlalchemy import SQLAlchemy
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="lucasamos2",
@@ -11,4 +10,3 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
-db = SQLAlchemy(app)
