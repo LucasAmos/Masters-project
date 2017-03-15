692e3b95669f5a43ebb077eac21bf995cc6ffe9f
lucasamos <Fujifilm12>
2017-03-15 22:39:12 +0000

    database and view

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
+
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
--- sensors/views.py
+++ sensors/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, url_for, redirect, request
 
 from sensors import app, db
 
@@ -9,6 +9,18 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-@app.route("/")
-def hello():
-        return render_template("main_page.html")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route("/wibbly")
+def wibbly():
+        return "my pointless route"
