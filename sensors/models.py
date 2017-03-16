7d1cef4fdac110f3003ba713a6209dd423dae947
lucasamos <Fujifilm12>
2017-03-16 23:23:21 +0100

    implemented api

--- sensors/models.py
+++ sensors/models.py
@@ -1,11 +1,16 @@
 from sensors import db
+from sqlalchemy import DateTime
+import datetime
 
 
 class Sensordata(db.Model):
 
     __tablename__ = "Sensordata"
 
+    dt = datetime.datetime.utcnow()
+
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(DateTime, default=dt)
     temperature = db.Column(db.Float)
     pressure = db.Column(db.Float)
     light = db.Column(db.Float)
--- sensors/views.py
+++ sensors/views.py
@@ -1,10 +1,10 @@
 from flask import render_template, url_for, redirect, request
-
 from sensors import app, db
-
 from models import Sensordata
+from flask_restful import Resource, Api
+from datetime import datetime
 
-
+api = Api(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,12 +12,49 @@ def index():
     if request.method == "GET":
         return render_template("main_page.html", readings=Sensordata.query.all())
 
-    reading = Sensordata(temperature=request.form["temperature"], light=request.form["light"], pressure=request.form["pressure"])
+    reading = Sensordata(temperature=request.form["temperature"], light=request.form["light"],
+                         pressure=request.form["pressure"])
     db.session.add(reading)
     db.session.commit()
     return redirect(url_for('index'))
 
 
-@app.route("/wibbly")
-def wibbly():
-        return "my pointless route"
+class AddSensorReading(Resource):
+
+    def get(self):
+        committedreading = db.session.query(Sensordata).order_by(Sensordata.id.desc()).first()
+
+        return {'time': committedreading.time.strftime('%B %d %Y - %H:%M:%S'),
+                'temperature': committedreading.temperature,
+                'pressure': committedreading.pressure, 'light': committedreading.light}
+
+    def put(self):
+        reading = Sensordata(time=datetime.utcnow(), temperature=request.form['temperature'],
+                             pressure=request.form['pressure'], light=request.form['light'])
+        db.session.add(reading)
+        db.session.commit()
+
+        committedreading = db.session.query(Sensordata).order_by(Sensordata.id.desc()).first()
+
+        return {'time': committedreading.time.strftime('%B %d %Y - %H:%M:%S'),
+                'temperature': committedreading.temperature,
+                'pressure': committedreading.pressure, 'light': committedreading.light}
+
+api.add_resource(AddSensorReading, '/addreading')
+
+# example api usage
+# put('http://localhost:8080/todo1', data={'temperature': '3', 'pressure': '5', 'light' : '6'}).json()
+
+
+class GetSensorReading(Resource):
+
+    def get(self):
+        committedreading = db.session.query(Sensordata).order_by(Sensordata.id.desc()).first()
+
+        return {'time': committedreading.time.strftime('%B %d %Y - %H:%M:%S'),
+                'temperature': committedreading.temperature,
+                'pressure': committedreading.pressure, 'light': committedreading.light}
+
+
+api.add_resource(GetSensorReading, '/getreading')
+
