fa0dbfd87f0400ad74a1ea679ad466024787d6da
lucasamos <ldja@st-andrews.ac.uk>
2017-06-14 01:27:25 +0100

    refactored API

--- sensors/views.py
+++ sensors/views.py
@@ -1,10 +1,10 @@
-from flask import render_template, request, jsonify, json
+from schemas import ma, Sensordatas_schema
+from flask import render_template, request, jsonify, abort, make_response
 from sensors import app, db
 from models import Sensordata
 from flask_restful import Resource, Api
 from datetime import datetime
 api = Api(app)
-from schemas import ma, Sensordatas_schema
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -25,9 +25,12 @@ def visualisation():
 # return a reading given it's ID
 # requests.get('http://localhost:8080/readings/21').json()
 @app.route('/readings/<reading_id>', methods=['GET'])
-def get_reading(reading_id):
+def get_reading_by_id(reading_id):
 
     reading = db.session.query(Sensordata).get(reading_id)
+
+    if reading is None:
+        abort(404)
 
     return jsonify({'DateTime': reading.time.strftime('%d/%m/%Y  %H:%M:%S'),
                     'temperature': reading.temperature,
@@ -56,13 +59,12 @@ def get_readings():
 
 
 # return a json array of all readings between a two dates (inclusive at start, exclusive at end)
-@app.route('/range/<start>/<end>', methods=['GET'])
+@app.route('/readings/<start>/<end>', methods=['GET'])
 def range(start, end):
     start = datetime.strptime(start, '%d %m %Y %H:%M:%S')
     end = datetime.strptime(end, '%d %m %Y %H:%M:%S')
 
     sensordata = db.session.query(Sensordata).filter(Sensordata.time >= start, Sensordata.time <= end).all()
-
     data_array = []
 
     for data in sensordata:
@@ -77,43 +79,59 @@ def range(start, end):
     return jsonify(data_array)
 
 
+# return the most recent reading in the database
+@app.route('/reading', methods=['GET'])
+def reading_latest():
+    data = db.session.query(Sensordata).order_by(Sensordata.id.desc()).first()
+
+    reading = {'id': data.id,
+               'deviceid' : data.DeviceID,
+               "DateTime": data.time.strftime('%d/%m/%Y  %H:%M:%S'),
+               'temperature': data.temperature,
+               'pressure': data.pressure,
+               'light': data.light
+               }
+
+    return jsonify(reading)
+
+
+@app.route('/add', methods=['POST'])
+def add_reading():
+
+    print request.json()
+
+    reading = {'a': 'b'}
+    return jsonify(reading)
+
+
+# add reading and return what was commmitted to the database.
+# requests.post('http://localhost:8080/addreading', data={'temperature': '3', 'pressure': '5', 'light' : '6', 'device': 'PiA'}).json()
+
 class AddSensorReading(Resource):
 
-    def get(self):
-        reading = db.session.query(Sensordata).order_by(Sensordata.id.desc()).first()
-
-        return {'DateTime': reading.time.strftime('%B %d %Y - %H:%M:%S'),
-                'temperature': reading.temperature,
-                'pressure': reading.pressure, 'light': reading.light}
-
     def post(self):
+
+        if not 'device' in request.form:
+           abort(400)
+
         reading = Sensordata(time=datetime.utcnow(), temperature=request.form['temperature'],
-                             pressure=request.form['pressure'], light=request.form['light'])
+                             pressure=request.form['pressure'], light=request.form['light'],
+                             DeviceID=request.form['device'])
         db.session.add(reading)
         db.session.commit()
 
-        committedreading = db.session.query(Sensordata).order_by(Sensordata.id.desc()).first()
-
-        return {'time': committedreading.time.strftime('%B %d %Y - %H:%M:%S'),
-                'temperature': committedreading.temperature,
-                'pressure': committedreading.pressure, 'light': committedreading.light}
+        return {'id': reading.id,
+                'deviceid' : reading.DeviceID,
+                "DateTime": reading.time.strftime('%d/%m/%Y  %H:%M:%S'),
+                'temperature': reading.temperature,
+                'pressure': reading.pressure,
+                'light': reading.light
+                }
 
 api.add_resource(AddSensorReading, '/addreading')
 
-# example api usage
-# requests.put('http://localhost:8080/addreading', data={'temperature': '3', 'pressure': '5', 'light' : '6'}).json()
 
-
-class GetSensorReadings(Resource):
-
-    def get(self):
-        committedreading = db.session.query(Sensordata).order_by(Sensordata.id.desc()).first()
-
-        return {'time': committedreading.time.strftime('%B %d %Y - %H:%M:%S'),
-                'temperature': committedreading.temperature,
-                'pressure': committedreading.pressure, 'light': committedreading.light}
-
-
-api.add_resource(GetSensorReadings, '/getreadings')
-# example api usage
-# requests.get('http://localhost:8080/getreading').json()
+# return json encoded 404 error
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
