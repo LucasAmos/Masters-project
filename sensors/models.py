cfe66b5688f489d22ebcec1ef1968ceae1f09f3b
lucasamos <ldja@st-andrews.ac.uk>
2017-06-27 21:46:36 +0100

    updated model

--- sensors/models.py
+++ sensors/models.py
@@ -11,9 +11,7 @@ class Sensordata(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(DateTime, default=dt)
-    temperature = db.Column(db.Float)
     pressure = db.Column(db.Float)
-    light = db.Column(db.Float)
     voc = db.Column(db.Float)
     DeviceID = db.Column(db.TEXT)
     dht11 = db.Column(db.Float)
