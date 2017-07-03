72f0914e93c356288f7813033443ae1253041d88
lucasamos <ldja@st-andrews.ac.uk>
2017-07-03 22:17:15 +0100

    refactored queries

--- sensors/views.py
+++ sensors/views.py
@@ -15,12 +15,15 @@ auth =HTTPBasicAuth()
 def index():
     if request.method == "GET":
 
-        return render_template("main_page.html", readings=Sensordata.query.order_by(Sensordata.time.desc()).limit(60).all())
+        return render_template("main_page.html",
+                               readings=db.session.query(Sensordata).order_by(Sensordata.time.desc()).limit(60).all())
+
+    # sensordata = db.session.query(Sensordata).filter(Sensordata.time >= start, Sensordata.time <= end, Sensordata.DeviceID == deviceid).all()
 
 
 @app.route("/visualisation")
 def visualisation():
-    readings = Sensordata.query.limit(2400).all()
+    readings = db.session.query(Sensordata).order_by(Sensordata.time.desc()).limit(2400).all()
     data, errors = Sensordatas_schema.dump(readings)
 
     return render_template('visualisation3.html', data3 = data)
