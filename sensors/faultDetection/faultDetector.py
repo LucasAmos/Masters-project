24fcdf0ddbd2359f0955e5ba0bb4b8ef58607ec2
lucasamos <ldja@st-andrews.ac.uk>
2017-07-17 23:08:53 +0100

    dht11

--- sensors/faultDetection/faultDetector.py
+++ sensors/faultDetection/faultDetector.py
@@ -2,8 +2,6 @@ from datetime import  datetime
 
 def correctfault(readings):
 
-    start = datetime.now()
-
 
     # do not iterate over the first and last elements
     for idx, reading in enumerate(readings[1:-1]):
@@ -18,12 +16,7 @@ def correctfault(readings):
 
             # if the the difference between an element and its preceding element is greater than 5:
             elif (float(readings[idx + 1]['dht22']) - float(readings[idx]['dht22'])) > 5:
-                # print("ran")
-                # print(float(readings[idx + 1]['dht22']) - float(readings[idx]['dht22']))
-                # print("idx-1: " + str(readings[idx ]['dht22']))
-                # print("idx: " + str(readings[idx + 1]['dht22']))
-                # print("")
-                #replace the element with the preceding element
+
                 readings[idx ]['dht22'] = float(readings[idx-1]['dht22'])
 
 
@@ -42,16 +35,18 @@ def correctfault(readings):
                 readings[idx]['humidity'] = float(readings[idx - 1]['humidity'])
 
 
-        if float(readings[idx]['dht11'] is not None):
+        if float(readings[idx+1]['dht11'] is not None):
 
+            # assumes that the first reading is not an error. If the element is a clear error:
             if float(readings[idx+1]['dht11']) > 50 or float(readings[idx+1]['dht11']) < 10:
+
+                # replace the element with the previous element (this is why it's import first element is not an error
                 readings[idx + 1]['dht11'] = float(readings[idx]['dht11'])
 
-            elif 50 < float(readings[idx + 1]['dht11']) - float(readings[idx + 2]['dht11']) > 5:
-                readings[idx + 2]['dht11'] = float(readings[idx + 1]['dht11'])
+            # if the the difference between an element and its preceding element is greater than 5:
+            elif (float(readings[idx + 1]['dht11']) - float(readings[idx]['dht11'])) > 5:
 
-    end = datetime.now()
+                readings[idx ]['dht11'] = float(readings[idx-1]['dht11'])
 
-    print((end - start).total_seconds())
 
     return readings
