67f15a060764b41ca0f6e6a2de5f04ea98485cd2
lucasamos <ldja@st-andrews.ac.uk>
2017-07-18 03:01:33 +0100

    fault logic

--- sensors/faultDetection/faultDetector.py
+++ sensors/faultDetection/faultDetector.py
@@ -15,7 +15,7 @@ def correctfault(readings):
                 readings[idx + 1]['dht22'] = float(readings[idx]['dht22'])
 
             # if the the difference between an element and its preceding element is greater than 5:
-            elif (float(readings[idx ]['dht22']) - float(readings[idx +1]['dht22'])) > 5:
+            elif (float(readings[idx + 1]['dht22']) - float(readings[idx]['dht22'])) > 5:
 
                 readings[idx ]['dht22'] = float(readings[idx-1]['dht22'])
 
