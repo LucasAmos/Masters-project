6c1c5bbf50b5ff608f74280229caf3ec1455c04b
lucasamos <ldja@st-andrews.ac.uk>
2017-07-31 15:16:12 +0100

    abstracted fault detector for temperature sensor

--- sensors/views.py
+++ sensors/views.py
@@ -63,7 +63,7 @@ def visualisation():
 
     JCLab, errors2 = Sensordatas_schema.dump(readings2)
 
-    #JCLab = correctfault2(JCLab)
+    JCLab = correctfault2(JCLab, "dht11")
 
     readings3 = db.session.query(Sensordata).filter(
         Sensordata.DeviceID == "PiJHLabDoor",
