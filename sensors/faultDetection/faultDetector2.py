02d2c3f3c4be527febe2f39f64744b5ebeb9fba5
lucasamos <ldja@st-andrews.ac.uk>
2017-07-31 16:37:40 +0100

    abstracted fault detector for temperature sensor

--- sensors/faultDetection/faultDetector2.py
+++ sensors/faultDetection/faultDetector2.py
@@ -39,7 +39,9 @@ def correctfault2(readings, sensorid):
                 readings[idx + 1]['humidity'] = float(readings[idx]['humidity'])
 
             # if the the difference between an element and its preceding element is greater than 5:
-            elif (float(readings[idx + 1]['humidity']) - float(readings[idx]['humidity'])) > humidityvariance/3:
+            elif (float(readings[idx + 1]['humidity']) - float(readings[idx]['humidity'])) > 5:
+
+                print("humidty variance: " + str(humidityvariance))
 
                 print (
                 "idx " + str(float(readings[idx]['humidity'])) + " idx+1 " + str(float(readings[idx + 1]['humidity'])))
