083b067406eafe97e85191aebf40a9541cc60627
lucasamos <ldja@st-andrews.ac.uk>
2017-07-31 15:03:25 +0100

    fault test

--- sensors/faultDetection/faultDetector2.py
+++ sensors/faultDetection/faultDetector2.py
@@ -39,11 +39,11 @@ def correctfault2(readings):
     for idx, reading in enumerate(readings[1:-1]):
         if float(readings[idx+1]['dht22'] is not None):
 
-            # # assumes that the first reading is not an error. If the element is a clear error:
-            # if float(readings[idx+1]['dht22']) > 50 or float(readings[idx+1]['dht22']) < 10:
-            #
-            #     # replace the element with the previous element (this is why it's import first element is not an error
-            #     readings[idx + 1]['dht22'] = float(readings[idx]['dht22'])
+            # assumes that the first reading is not an error. If the element is a clear error:
+            if float(readings[idx+1]['dht22']) > (dht22mean + (dht22variance * 2)) or float(readings[idx+1]['dht22']) < (dht22mean - (dht22variance * 2)):
+
+                # replace the element with the previous element (this is why it's import first element is not an error
+                readings[idx + 1]['dht22'] = float(readings[idx]['dht22'])
 
             # if the the difference between an element and its preceding element is greater than 5:
             if (float(readings[idx + 1]['dht22']) - float(readings[idx]['dht22'])) > dht22variance/2:
