8afd1114476a6ddb11dad142bf3e5ad8e4aa2d61
lucasamos <ldja@st-andrews.ac.uk>
2017-07-31 16:05:58 +0100

    abstracted fault detector for temperature sensor

--- sensors/faultDetection/faultDetector2.py
+++ sensors/faultDetection/faultDetector2.py
@@ -2,36 +2,25 @@ import math
 
 
 def correctfault2(readings, sensorid):
-    dht22count= 0
     humiditycount = 0
 
     for reading in readings:
-
-
-        dht22count += float(reading['dht22'])
         humiditycount += float(reading['humidity'])
 
-    dht22mean = dht22count / len(readings)
     humiditymean = humiditycount / len(readings)
 
 
-    dht22variance = 0
     humidityvariance = 0
 
     for reading in readings:
-        dht22variance += (float(reading['dht22']) - dht22mean) ** 2
         humidityvariance += (float(reading['dht22']) - humiditymean) ** 2
 
 
-    dht22variance = dht22variance / (len(readings) - 1)
-    dht22variance = math.sqrt(dht22variance)
 
     humidityvariance = humidityvariance / (len(readings) - 1)
     humidityvariance = math.sqrt(humidityvariance)
 
-    print("")
-    print ("dht22 mean: " + str(dht22mean))
-    print ("dht22 variance: " + str(dht22variance))
+
     print ("humidity mean: " + str(humiditymean))
     print ("humidity variance: " + str(humidityvariance))
 
@@ -55,9 +44,21 @@ def correctfault2(readings, sensorid):
                             # replace the element with the preceding element
                 readings[idx]['humidity'] = float(readings[idx - 1]['humidity'])
 
-
-
     if sensorid == 22:
+
+        dht22count = 0
+        dht22variance = 0
+
+        for reading in readings:
+            dht22count += float(reading['dht22'])
+
+        dht22mean = dht22count / len(readings)
+
+        for reading in readings:
+            dht22variance += (float(reading['dht22']) - dht22mean) ** 2
+
+        dht22variance = dht22variance / (len(readings) - 1)
+        dht22variance = math.sqrt(dht22variance)
 
         print("id 22")
 
@@ -82,7 +83,45 @@ def correctfault2(readings, sensorid):
 
                     readings[idx+1]['dht22'] = float(readings[idx]['dht22'])
 
+    if sensorid == 11:
 
+        dht11count = 0
+        dht11variance = 0
+
+        for reading in readings:
+            dht11count += float(reading['dht11'])
+
+        dht11mean = dht11count / len(readings)
+
+        for reading in readings:
+            dht11variance += (float(reading['dht11']) - dht11mean) ** 2
+
+        dht11variance = dht11variance / (len(readings) - 1)
+        dht11variance = math.sqrt(dht11variance)
+
+        print("id 11")
+
+        for idx, reading in enumerate(readings[1:-1]):
+
+            if float(readings[idx + 1]['dht11'] is not None):
+
+                # assumes that the first reading is not an error. If the element is a clear error:
+                if float(readings[idx + 1]['dht11']) > (dht11mean + (dht11variance * 2)) or float(
+                        readings[idx + 1]['dht11']) < (dht11mean - (dht11variance * 2)):
+                    # replace the element with the previous element (this is why it's import first element is not an error
+                    readings[idx + 1]['dht11'] = float(readings[idx]['dht11'])
+
+                # if the the difference between an element and its preceding element is greater than 5:
+                if (float(readings[idx + 1]['dht11']) - float(
+                        readings[idx]['dht11'])) > dht11variance / 2:
+
+                    readings[idx]['dht11'] = float(readings[idx - 1]['dht11'])
+
+                # if the the difference between an element and its next element is greater than 5:
+                elif (float(readings[idx]['dht11']) - float(
+                        readings[idx + 1]['dht11'])) > dht11variance / 2:
+
+                    readings[idx + 1]['dht11'] = float(readings[idx]['dht11'])
 
 
 
