6a634075f26c03299a0162c214ea2f335bb993d8
lucasamos <ldja@st-andrews.ac.uk>
2017-07-31 15:04:49 +0100

    fault test

--- sensors/faultDetection/faultDetector2.py
+++ sensors/faultDetection/faultDetector2.py
@@ -75,18 +75,18 @@ def correctfault2(readings):
         if float(readings[idx+1]['dht11'] is not None):
 
             # assumes that the first reading is not an error. If the element is a clear error:
-            if float(readings[idx+1]['dht11']) > 50 or float(readings[idx+1]['dht11']) < 10:
+            if float(readings[idx+1]['dht11']) > (dht22mean + (dht22variance * 2)) or float(readings[idx+1]['dht11']) < (dht22mean - (dht22variance * 2)):
 
                 # replace the element with the previous element (this is why it's import first element is not an error
                 readings[idx + 1]['dht11'] = float(readings[idx]['dht11'])
 
             # if the the difference between an element and its preceding element is greater than 5:
-            elif (float(readings[idx + 1]['dht11']) - float(readings[idx]['dht11'])) > 3:
+            if (float(readings[idx + 1]['dht11']) - float(readings[idx]['dht11'])) > dht22variance/2:
 
                 readings[idx ]['dht11'] = float(readings[idx-1]['dht11'])
 
             # if the the difference between an element and its next element is greater than 5:
-            elif (float(readings[idx ]['dht11']) - float(readings[idx + 1]['dht11'])) > 3:
+            elif (float(readings[idx ]['dht11']) - float(readings[idx + 1]['dht11'])) > dht22variance/2:
 
                 readings[idx+1]['dht11'] = float(readings[idx]['dht11'])
 
