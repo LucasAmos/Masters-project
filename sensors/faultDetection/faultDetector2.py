558000eb00460bb1c225a37ca2c19ca64cdfc019
lucasamos <ldja@st-andrews.ac.uk>
2017-08-02 12:53:10 +0100

    pre processing

--- sensors/faultDetection/faultDetector2.py
+++ sensors/faultDetection/faultDetector2.py
@@ -26,15 +26,14 @@ def correctfault2(readings, sensorid):
             # assumes that the first reading is not an error. If the element is a clear error:
             if float(readings[idx + 1]['humidity']) > 70 or float(readings[idx + 1]['humidity']) < humidityvariance*2:
 
-
                 # replace the element with the previous element (this is why it's import first element is not an error
                 readings[idx + 1]['humidity'] = float(readings[idx]['humidity'])
 
-            # if the the difference between an element and its preceding element is greater than 5:
-            elif (float(readings[idx + 1]['humidity']) - float(readings[idx]['humidity'])) > humidityvariance/2:
-
-                # replace the element with the preceding element
-                readings[idx]['humidity'] = float(readings[idx - 1]['humidity'])
+            # # if the the difference between an element and its preceding element is greater than 5:
+            # elif (float(readings[idx + 1]['humidity']) - float(readings[idx]['humidity'])) > humidityvariance/2:
+            #
+            #     # replace the element with the preceding element
+            #     readings[idx]['humidity'] = float(readings[idx - 1]['humidity'])
 
     if sensorid == 22:
 
