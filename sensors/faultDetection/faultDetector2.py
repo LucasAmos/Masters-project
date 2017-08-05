5f28d253414cf91814a70851997b1aabed40ca95
lucasamos <ldja@st-andrews.ac.uk>
2017-08-05 14:54:16 +0100

    altered code to exclude outliers from variance

--- sensors/faultDetection/faultDetector2.py
+++ sensors/faultDetection/faultDetector2.py
@@ -77,12 +77,12 @@ def correctfault2(readings, sensorid):
                 # if the the difference between an element and its preceding element is greater than 5:
                 if (float(readings[idx + 1]['dht22']) - float(readings[idx]['dht22'])) > dht22variance/2:
 
-                    readings[idx +1]['dht22'] = float(readings[idx]['dht22'])
+                    readings[idx +1]['dht22'] = ((float(readings[idx]['dht22']) + float(readings[idx+2]['dht22']))/2)
 
                 # if the the difference between an element and its next element is greater than 5:
                 elif (float(readings[idx ]['dht22']) - float(readings[idx + 1]['dht22'])) > dht22variance/2:
 
-                    readings[idx+1]['dht22'] = float(readings[idx]['dht22'])
+                    readings[idx +1]['dht22'] = ((float(readings[idx]['dht22']) + float(readings[idx+2]['dht22']))/2)
 
     elif sensorid == 11:
 
