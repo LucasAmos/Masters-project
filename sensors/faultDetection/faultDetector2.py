4595f2069b7bd4b7964e8ebb5bfcd6bc0459b364
lucasamos <ldja@st-andrews.ac.uk>
2017-08-06 15:05:02 +0100

    altered code to exclude outliers from variance

--- sensors/faultDetection/faultDetector2.py
+++ sensors/faultDetection/faultDetector2.py
@@ -34,13 +34,13 @@ def correctfault2(readings, sensorid):
             #     # replace the element with the previous element (this is why it's import first element is not an error
             #     readings[idx + 1]['humidity'] = float(readings[idx]['humidity'])
 
-            # # if the the difference between an element and its preceding element is greater than 5:
-            # if (float(readings[idx + 1]['humidity']) - float(readings[idx]['humidity'])) > humidityvariance / 2:
-            #     readings[idx+1]['humidity'] = float(readings[idx]['humidity'])
+            # if the the difference between an element and its preceding element is greater than 5:
+            if (float(readings[idx + 1]['humidity']) - float(readings[idx]['humidity'])) > humidityvariance / 2:
+                readings[idx+1]['humidity'] = float(readings[idx]['humidity'])
 
 
             # if the the difference between an element and its preceding element is greater than 5:
-            if (float(readings[idx]['humidity']) - float(readings[idx + 1]['humidity'])) > humidityvariance / 2:
+            elif (float(readings[idx]['humidity']) - float(readings[idx + 1]['humidity'])) > humidityvariance / 2:
 
                 # replace the element with the preceding element
                 readings[idx + 1]['humidity'] = float(readings[idx]['humidity'])
